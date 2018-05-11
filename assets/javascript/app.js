cbcd812d1fdd8e313e3cf25410dc7650b624fe86
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-05-11 16:35:23 -0700

    Updated JS not fully functional yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 // Button Listener
-$("btn-submit").on("click", function(event) {
+$("#btn-submit").on("click", function() {
+  console.log("hello")
   event.preventDefault();
 
   // Grab user input
@@ -39,6 +40,8 @@ $("btn-submit").on("click", function(event) {
   console.log(newUser.illness);
   console.log(newUser.sort);
   console.log(newUser.gender);
+  
+  getLocation();
 
   // Clears all of the text-boxes
   $("#input-symptoms").val("");
@@ -47,7 +50,6 @@ $("btn-submit").on("click", function(event) {
   $("#sort-input").val("");
   $("#gender-input").val("");
 
-  getLocation();
 
 });
 
@@ -64,6 +66,28 @@ function getLocation(){
 
 
 var betterDoctor = function(position){
+  
+  var sortPreference = $("#sort-input").val().trim();
+  var doctorGender = $("#gender-input").val().trim();
+
+  var genderDoc = "";
+
+  var sortDoc = "";
+
+  if(doctorGender === 1){
+    genderDoc = "male";
+  }else if(doctorGender === 2){
+    genderDoc = "female";
+  }
+
+  if(sortPreference === 1){
+    sortDoc  = "best-match-desc";
+  }else if(sortPreference === 2){
+    sortDoc  = "rating-desc";
+  }else if(sortPreference === 3){
+    sortDoc  = "distance-asc";
+  }
+
 
   var key = "bc7f67f5ab920635890a971a98eac05e";
 
@@ -71,17 +95,17 @@ var betterDoctor = function(position){
 
   var lng = position.coords.longitude.toFixed(3);
 
-  var range = newUser.radar;
+  var range = $("#search-input").val().trim();
 
-  var num = newUser.num;
+  var num = $("#number-Of-Doc").val().trim();
 
-  var injury = newUser.illness;
+  var injury = $("#input-symptoms").val().trim();
 
-  var gender = newUser.gender;
+  var gender = genderDoc;
 
-  var sort = newUser.sort;
+  var sort = sortDoc;
 
-  var queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?query=" + injury + "location=" + lat + "," + lng + "," + range + "&skip=0&gender=" + gender "&sort=" + sort + "&limit=" + num + "&user_key=" + key;
+  var queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?query=" + injury + "&location=" + lat + "," + lng + "," + range + "&skip=0&gender=" + gender + "&sort=" + sort + "&limit=" + num + "&user_key=" + key;
 
   $.ajax({
     url: queryURL,
@@ -103,25 +127,25 @@ var betterDoctor = function(position){
       for(var j =0; j < response.data[i].specialties.length; j++){
         specArr.push(response.data[i].specialties[j].description)
       }
-      var specs[i] = specArr.concat(', ')
+      specs[i] = specArr.concat(', ')
 
       //Loop through insurances
       for(var k =0; k< response.data[i].insurances.length; k++){
         insuArr.push(response.data[i].insurances[k].insurance_plan.name)
       }
-      var ins[i] = insuArr.concat(', ')
+      ins[i] = insuArr.concat(', ')
 
       //Loop through practices
       for(var z =0; z< response.data[i].practices.length; z++){
         practArr.push(response.data[i].practices[z].steet + " " + response.data[i].practices[z].city + ", " + response.data[i].practices[z].state)
       }
-      var loc[i] = practArr.concat(', ')
+      loc[i] = practArr.concat(', ')
 
       //Loop through practices
       for(var y =0; y< response.data[i].phones.length; y++){
         practArr.push(response.data[i].phones[y].number)
       }
-      var tels[i] = practArr.concat(', ')
+      tels[i] = practArr.concat(', ')
 
       $("#table-list > tbody").append("<tr><td>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + ", " + response.data[i].profile.title + "</td><td>" + specs[i] + "</td><td>" +
       ins[i] + "</td><td>" + loc[i] + "</td><td>" + tels[i] + "</td></tr>");
