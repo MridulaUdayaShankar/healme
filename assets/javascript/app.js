720672461b95d4c038f9c666f14da160c89127b3
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-05-11 19:53:59 -0700

    Working MVP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,58 +3,12 @@ $("#btn-submit").on("click", function() {
   console.log("hello")
   event.preventDefault();
 
-  // Grab user input
-  var distanceFrom  = $("#input-symptoms").val().trim();
-  var displayList = $("#number-Of-Doc").val().trim();
-  var injuryEvent = $("#search-input").val().trim();
-  var sortPreference = $("#sort-input").val().trim();
-  var doctorGender = $("#gender-input").val().trim();
-
-  // Translate Option Menus to Text
-  if(doctorGender === 1){
-    doctorGender = "male";
-  }else if(doctorGender === 2){
-    doctorGender = "female";
-  }
-
-  if(sortPreference === 1){
-    sortPreference = "best-match-desc";
-  }else if(sortPreference === 2){
-    sortPreference = "rating-desc";
-  }else if(sortPreference === 3){
-    sortPreference = "distance-asc";
-  }
-
-  // Creates local "temporary" object for holding user data
-  var newUser = {
-    radar: distanceFrom,
-    num: displayList,
-    illness: injuryEvent,
-    sort: sortPreference,
-    gender: doctorGender
-  };
- 
-  // Logs everything to console
-  console.log(newUser.radar);
-  console.log(newUser.num);
-  console.log(newUser.illness);
-  console.log(newUser.sort);
-  console.log(newUser.gender);
-  
   getLocation();
-
-  // Clears all of the text-boxes
-  $("#input-symptoms").val("");
-  $("#number-Of-Doc").val("");
-  $("#search-input").val("");
-  $("#sort-input").val("");
-  $("#gender-input").val("");
-
 
 });
 
 
-function getLocation(){
+var getLocation = function(){
 
   if (navigator.geolocation){
     console.log(navigator.geolocation.getCurrentPosition(betterDoctor));
@@ -67,28 +21,6 @@ function getLocation(){
 
 var betterDoctor = function(position){
   
-  var sortPreference = $("#sort-input").val().trim();
-  var doctorGender = $("#gender-input").val().trim();
-
-  var genderDoc = "";
-
-  var sortDoc = "";
-
-  if(doctorGender === 1){
-    genderDoc = "male";
-  }else if(doctorGender === 2){
-    genderDoc = "female";
-  }
-
-  if(sortPreference === 1){
-    sortDoc  = "best-match-desc";
-  }else if(sortPreference === 2){
-    sortDoc  = "rating-desc";
-  }else if(sortPreference === 3){
-    sortDoc  = "distance-asc";
-  }
-
-
   var key = "bc7f67f5ab920635890a971a98eac05e";
 
   var lat = position.coords.latitude.toFixed(3);
@@ -101,9 +33,9 @@ var betterDoctor = function(position){
 
   var injury = $("#input-symptoms").val().trim();
 
-  var gender = genderDoc;
+  var gender = $("#gender-input").val().trim();
 
-  var sort = sortDoc;
+  var sort = $("#sort-input").val().trim();
 
   var queryURL = "https://api.betterdoctor.com/2016-03-01/doctors?query=" + injury + "&location=" + lat + "," + lng + "," + range + "&skip=0&gender=" + gender + "&sort=" + sort + "&limit=" + num + "&user_key=" + key;
 
@@ -113,45 +45,18 @@ var betterDoctor = function(position){
   }).then(function(response){
     console.log(response);
 
-    var specArr = [];
-    var insuArr = [];
-    var practArr = [];
-    var phoneArr = [];
-    var specs = [];
-    var ins = [];
-    var loc = [];
-    var tel = [];
-
-    for(var i =0; i< response.length ; i++){
-      //Loop through specialties
-      for(var j =0; j < response.data[i].specialties.length; j++){
-        specArr.push(response.data[i].specialties[j].description)
-      }
-      specs[i] = specArr.concat(', ')
-
-      //Loop through insurances
-      for(var k =0; k< response.data[i].insurances.length; k++){
-        insuArr.push(response.data[i].insurances[k].insurance_plan.name)
-      }
-      ins[i] = insuArr.concat(', ')
-
-      //Loop through practices
-      for(var z =0; z< response.data[i].practices.length; z++){
-        practArr.push(response.data[i].practices[z].steet + " " + response.data[i].practices[z].city + ", " + response.data[i].practices[z].state)
-      }
-      loc[i] = practArr.concat(', ')
-
-      //Loop through practices
-      for(var y =0; y< response.data[i].phones.length; y++){
-        practArr.push(response.data[i].phones[y].number)
-      }
-      tels[i] = practArr.concat(', ')
-
-      $("#table-list > tbody").append("<tr><td>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + ", " + response.data[i].profile.title + "</td><td>" + specs[i] + "</td><td>" +
-      ins[i] + "</td><td>" + loc[i] + "</td><td>" + tels[i] + "</td></tr>");
+    for(var i=0; i< response.meta.count; i++){
+      $("#table-list > tbody").append("<tr><td>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + ", " + response.data[i].profile.title + "</td><td>" + response.data[i].specialties[0].description + "</td><td>" + response.data[i].practices[0].visit_address.street + " " + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state +  " " + response.data[i].practices[0].visit_address.zip + "</td><td>" + response.data[i].practices[0].phones[0].number + "</td><td>" + response.data[i].practices[0].website + "</td></tr>");
     }
-
+  
   });
+
+    //Clears all of the text-boxes
+  $("#input-symptoms").val("");
+  $("#number-Of-Doc").val("");
+  $("#search-input").val("");
+  $("#sort-input").val("");
+  $("#gender-input").val("");
 }
 
 
